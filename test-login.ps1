eba8255da0b23d7fdcb456753f17dcbd47e46b97
2300031507 <2300031507@kluniversity.in>
2025-09-27 08:34:09 +0530

    Remove test files

--- test-login.ps1
+++
@@ -1,2 +0,0 @@
-$body = '{"email": "admin@example.com", "password": "password"}';
-Invoke-RestMethod -Uri "http://localhost:8081/api/auth/login" -Method POST -ContentType "application/json" -Body $body;
